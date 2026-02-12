faa6211a53a259bdc881997aec690f9b052f60a2
Rjesh-kumar <rajeshkumarrout40gmail.com>
2026-02-12 08:04:10 +0530

    chore: modify cors in index

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ initializeDatabase();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://taskasana-fe.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/api/protected", verifyToken, (req, res) => {
